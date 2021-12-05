4c1778b54e24127b7c1695f623544e5a0b56adfd
Bharat Poptwani <bharat.p@goodworker.in>
2021-12-05 19:43:53 +0530

    added backdrop to ErrorModal component

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -11,17 +11,20 @@ const ErrorModal = (props) => {
   const { title, message, onClose } = props;
 
   return (
-    <Card className={styles.modal}>
-      <header className={styles.header}>
-        <h2>{title}</h2>
-      </header>
-      <div className={styles.content}>
-        <p>{message}</p>
-      </div>
-      <footer className={styles.actions}>
-        <Button>Okay</Button>
-      </footer>
-    </Card>
+    <div>
+      <div className={styles.backdrop} />
+      <Card className={styles.modal}>
+        <header className={styles.header}>
+          <h2>{title}</h2>
+        </header>
+        <div className={styles.content}>
+          <p>{message}</p>
+        </div>
+        <footer className={styles.actions}>
+          <Button>Okay</Button>
+        </footer>
+      </Card>
+    </div>
   );
 };
 
